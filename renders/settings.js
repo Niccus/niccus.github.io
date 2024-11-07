b25bf354bd863b437364800c19130361a25d6a96
niccus <neecous@gmail.com>
2024-11-06 20:06:55 -0600

    Digression: Projector Project

---
+++ renders/settings.js
@@ -0,0 +1,50 @@
+// Look, if you use a dirt cheap projector and aim it at a random window, it's going to be... very poorly fitting.
+// Assuming 1920x1080 window, parameters to align to the window and not have the wall around the window get too bright.
+// todo:? tilt parameters/rendering because the projector is waaaay at an angle
+
+const _top = 155;
+const _left = 100;
+const _canvas_top = `${_top.toString()}px`;
+const _canvas_left = `${_left.toString()}px`;
+
+const _h = 720;
+const _w = 1700;
+const _canvas_h = `${_h.toString()}px`;
+const _canvas_w = `${_w.toString()}px`;
+
+const tau = Math.PI * 2;
+
+//Helpers
+function clamp(from, to, now) {
+    return (now < from) ? from : (now < to) ? now : to;
+}
+
+function progress(from, to, now) {
+    return (clamp(from, to, now) - from) / (to - from);
+}
+
+function lerp( a, b, alpha ) {
+    return a + alpha * ( b - a );
+}
+
+function lerpt(a, b, from, to, now) {
+    return lerp(a, b, progress(from, to, now))
+}
+
+function r() {
+    return Math.random();
+}
+
+var sin = Math.sin;
+var cos = Math.cos;
+var abs = Math.abs;
+
+function rand2dir() {
+    let theta = Math.random() * tau;
+    return {x:Math.cos(theta), y:Math.sin(theta)};
+}
+
+function rand2rdir(r) {
+    let theta = Math.random() * tau;
+    return {x:r*Math.cos(theta), y:r*Math.sin(theta)};
+}
